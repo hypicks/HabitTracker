83896ce36f54b2bc6320361e60076b2cce330d15
hypicks <44602385+hypicks@users.noreply.github.com>
2022-05-20 12:48:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import datetime
+
+
+TOKEN = "wadawdijh21213"
+USERNAME = "hypixx"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# created username and password.
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Workout",
+    "unit": "Hours",
+    "type": "int",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+graph_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hours did you work out today?: "),
+}
+
+response = requests.post(url=graph_pixel_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "0",
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint= f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
